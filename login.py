b0651b37ee6bfd5d6f45180d3c113faacb7eb006
RamyaRaj14 <76992109+RamyaRaj14@users.noreply.github.com>
2021-01-05 13:41:14 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+#Login app
+username=input("Please Enter Valid USer Name:")
+password=input("Please Enter Valid Password:")
+if username=='ramya' and password=='ramya14':
+    print("Your User Name:",username)
+else:
+    print("Wrong Credentials")
+    print("bye")
